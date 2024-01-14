ff06e9592374d9877375fd9de01967254eba44e0
Ansarul Haq <mdansarul786940@gmail.com>
2024-01-14 22:04:41 +0545

    created a guess number game with suprising gift after sucessfull  trial

---
+++ script.js
@@ -0,0 +1,11 @@
+secretNumber=7;
+userNumber=parseInt(prompt("Guess the secret number:"));
+while(userNumber!==secretNumber)
+{
+    userNumber=parseInt(prompt("Your guess is wrong! Try again:"));
+}
+console.log("Congratulations! Here is your gift.");
+
+let linkElement = document.getElementById("randomlink");
+
+linkElement.href = "https://youtu.be/xvFZjo5PgG0?feature=shared&autoplay=1&mute=0";
